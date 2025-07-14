55bf430db8c59e13abc9ad831749db8c68e95d6b
suryavaddiraju <112264612+suryavaddiraju@users.noreply.github.com>
2025-07-14 14:34:06 +0000

    chore: update to 3.844.0

--- examples/main.mjs
+++ examples/main.mjs
@@ -1,3 +1,3 @@
-import {ClientCommand } from "@aws-sdk/{repoName}";
+import {ClientCommand } from "@aws-sdk/client-cognito-identity";
 
 // Refer "https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/" for documentation of this command
